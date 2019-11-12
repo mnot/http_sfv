8dd34043353eadae5151dbe78ec475e224d7cb4e
Mark Nottingham <mnot@mnot.net>
2019-11-12 16:17:08 +1100

    test parse and serialise

--- test/test.py
+++ test/test.py
@@ -5,9 +5,9 @@ from collections import OrderedDict
 import json
 from pathlib import Path
 import sys
-from typing import Any, List
+from typing import Any, List, Union
 
-from shhh import parse
+from shhh import parse, serialise
 
 FAIL = "\033[91m"
 ENDC = "\033[0m"
@@ -30,35 +30,67 @@ def run_suite(suite_name: str, suite: List) -> None:
     suite_passed = 0
     for test in suite:
         suite_tests += 1
-        parsed = None
-        parse_success = False
-        parse_fail_reason = None
-        test_success = False
-        try:
-            parsed = parse(", ".join(test["raw"]), test["header_type"])
-            parse_success = True
-        except ValueError as why:
-            parse_fail_reason = why
-        except Exception:
-            sys.stderr.write("*** TEST ERROR in %s\n" % test["name"])
-            raise
-        if test.get("must_fail", False):
-            test_success = not parse_success
-        else:
-            test_success = test["expected"] == walk_json(parsed)
-        if test_success:
+        parse_success, parsed, parse_fail_reason = test_parse(test)
+        ser_success = True
+        if not test.get("must_fail", False):
+            ser_success, serialised, ser_expected, ser_fail_reason = test_serialise(
+                test
+            )
+
+        if parse_success and ser_success:
             suite_passed += 1
         else:
-            print("%s  * %s: FAIL%s" % (FAIL, test["name"], ENDC))
-            print("    - expected: %s" % test.get("expected", "FAIL"))
-            print("    -      got: %s" % walk_json(parsed))
-            print("    -   reason: %s" % parse_fail_reason)
-        if not test_success and test.get("can_fail", False):
-            print("    - (test failure not critical)")
-            suite_passed += 1
-    print("-> %s of %s passed." % (suite_passed, suite_tests))
+            if not parse_success:
+                print(f"{FAIL}  * {test['name']}: PARSE FAIL{ENDC}")
+                print(f"    - expected: {test.get('expected', 'FAIL')}")
+                print(f"    -      got: {walk_json(parsed)}")
+                print(f"    -   reason: {parse_fail_reason}")
+                if test.get("can_fail", False):
+                    print("    - (test failure not critical)")
+            if not ser_success:
+                print(f"{FAIL} * {test['name']}: SERIALISE FAIL{ENDC}")
+                print(f"    - expected: {ser_expected}")
+                print(f"    -      got: {serialised}")
+                print(f"    -   reason: {ser_fail_reason}")
+
+    print(f"-> {suite_passed} of {suite_tests} passed.")
     print()
     return suite_tests, suite_passed
+
+
+def test_parse(test: dict) -> Union[bool, Any, str]:
+    parsed = None
+    parse_success = False
+    parse_fail_reason = None
+    test_success = False
+    try:
+        parsed = parse(", ".join(test["raw"]), test["header_type"])
+        parse_success = True
+    except ValueError as why:
+        parse_fail_reason = why
+    except Exception:
+        sys.stderr.write(f"*** TEST ERROR in {test['name']}\n")
+        raise
+    if test.get("must_fail", False):
+        test_success = not parse_success
+    else:
+        test_success = test["expected"] == walk_json(parsed)
+    return test_success, parsed, parse_fail_reason
+
+
+def test_serialise(test: dict) -> Union[bool, str, str, str]:
+    expected = test.get("canonical", test["raw"])
+    output = None
+    serialise_fail_reason = None
+    try:
+        output = serialise(test["expected"], test["header_type"])
+    except ValueError as why:
+        serialise_fail_reason = why
+    except Exception:
+        sys.stderr.write(f"*** TEST ERROR in {test['name']}\n")
+        raise
+    test_success = expected == [output]
+    return test_success, output, expected, serialise_fail_reason
 
 
 def walk_json(thing: Any) -> Any:
