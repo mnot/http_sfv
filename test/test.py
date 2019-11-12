8b0f14d547e588ccd462b7d9a76b11947020960d
Mark Nottingham <mnot@mnot.net>
2019-11-12 16:59:05 +1100

    walk tests for serialisation; use decimal for floats in json

--- test/test.py
+++ test/test.py
@@ -2,6 +2,7 @@
 
 import base64
 from collections import OrderedDict
+import decimal
 import json
 from pathlib import Path
 import sys
@@ -19,7 +20,7 @@ def load_tests(files=None) -> List:
         files = Path("test/tests").glob("*.json")
     for filename in files:
         fh = open(filename)
-        suite_json = json.load(fh)
+        suite_json = json.load(fh, parse_float=decimal.Decimal)
         suites.append((filename, suite_json))
     return suites
 
@@ -43,14 +44,14 @@ def run_suite(suite_name: str, suite: List) -> None:
             if not parse_success:
                 print(f"{FAIL}  * {test['name']}: PARSE FAIL{ENDC}")
                 print(f"    - expected: {test.get('expected', 'FAIL')}")
-                print(f"    -      got: {walk_json(parsed)}")
+                print(f"    -      got: {walk_json_parse(parsed)}")
                 print(f"    -   reason: {parse_fail_reason}")
                 if test.get("can_fail", False):
                     print("    - (test failure not critical)")
             if not ser_success:
                 print(f"{FAIL} * {test['name']}: SERIALISE FAIL{ENDC}")
                 print(f"    - expected: {ser_expected}")
-                print(f"    -      got: {serialised}")
+                print(f"    -      got: [{serialised}]")
                 print(f"    -   reason: {ser_fail_reason}")
 
     print(f"-> {suite_passed} of {suite_tests} passed.")
@@ -74,7 +75,7 @@ def test_parse(test: dict) -> Union[bool, Any, str]:
     if test.get("must_fail", False):
         test_success = not parse_success
     else:
-        test_success = test["expected"] == walk_json(parsed)
+        test_success = test["expected"] == walk_json_parse(parsed)
     return test_success, parsed, parse_fail_reason
 
 
@@ -82,8 +83,9 @@ def test_serialise(test: dict) -> Union[bool, str, str, str]:
     expected = test.get("canonical", test["raw"])
     output = None
     serialise_fail_reason = None
+    input_data = walk_json_ser(test["expected"])
     try:
-        output = serialise(test["expected"], test["header_type"])
+        output = serialise(input_data, test["header_type"])
     except ValueError as why:
         serialise_fail_reason = why
     except Exception:
@@ -93,14 +95,25 @@ def test_serialise(test: dict) -> Union[bool, str, str, str]:
     return test_success, output, expected, serialise_fail_reason
 
 
-def walk_json(thing: Any) -> Any:
+def walk_json_parse(thing: Any) -> Any:
     out = thing
     if type(thing) in [OrderedDict, dict]:
-        out = {k: walk_json(thing[k]) for k in thing}
+        out = {k: walk_json_parse(thing[k]) for k in thing}
     if type(thing) in [list, tuple]:
-        out = [walk_json(i) for i in thing]
+        out = [walk_json_parse(i) for i in thing]
     if type(thing) is bytes:
         out = base64.b32encode(thing).decode("ascii")
+    return out
+
+def walk_json_ser(thing: Any) -> Any:
+    out = thing
+    if type(thing) is dict:
+        out = {k: walk_json_ser(thing[k]) for k in thing}
+    if type(thing) is list:
+        out = [walk_json_ser(i) for i in thing]
+    if isinstance(thing, str):
+        if thing.endswith("=*"):
+            out = base64.b32decode(thing)
     return out
 
 
