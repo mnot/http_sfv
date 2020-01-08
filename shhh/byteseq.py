e39efd4c94fb579b4cb9ba96d6055cec8a5dffab
Mark Nottingham <mnot@mnot.net>
2020-01-08 12:58:43 +1100

    fix lingering "*"

--- shhh/byteseq.py
+++ shhh/byteseq.py
@@ -6,12 +6,12 @@ BYTE_DELIMIT = ":"
 
 
 def parse_byteseq(input_string: str) -> Tuple[str, bytes]:
-    if input_string and input_string[0] != "*":
+    if input_string and input_string[0] != BYTE_DELIMIT:
         raise ValueError(
             f"Binary Sequence didn't start with '{BYTE_DELIMIT}'.", input_string
         )
     input_string = input_string[1:]
-    if "*" not in input_string:
+    if BYTE_DELIMIT not in input_string:
         raise ValueError(
             f"Binary Sequence didn't contain ending '{BYTE_DELIMIT}'.", input_string
         )
