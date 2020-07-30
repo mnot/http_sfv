36b574b72d1e55f367f9d2207db25c711a48b78e
Mark Nottingham <mnot@mnot.net>
2020-07-30 14:12:29 +1000

    shown ns

--- test/test_perf.py
+++ test/test_perf.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env pypy3
 
+import locale
 import timeit
 
 import http_sfv as sfv
 
+locale.setlocale(locale.LC_ALL, "")
 
+digits = 9
+unit = 'ns'
+i = 50000
 
 perf_structures = [
     ('"abcdefghijklmnopqrstuvwxyz"', 'item', 'String (simple)'),
@@ -22,16 +27,15 @@ perf_structures = [
 
 
 def time_parse(structure, field_type):
-    i = 100000
-    return timeit.timeit(
-        f"sfv.structures['{field_type}']().parse('{structure}')",
+    return int(timeit.timeit(
+        f"sfv.structures['{field_type}']().parse(b'{structure}')",
         setup='import time; time.sleep(2)',
         globals=globals(),
         number=i
-    )
+    ) / i * (10 ** digits))
 
 
 if __name__ == "__main__":
     for test_structure, field_type, name in perf_structures:
         times = time_parse(test_structure, field_type)
-        print(f"* {name:25.25s}: {times:4.3f}s")
+        print(f"* {name:25.25s} {times:{digits}n} {unit}")
