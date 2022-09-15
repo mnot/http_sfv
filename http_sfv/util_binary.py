5a4079d4c3bd899dcefd55b29d00034c2715b168
Mark Nottingham <mnot@mnot.net>
2022-09-15 15:38:57 +1000

    rework

--- http_sfv/util_binary.py
+++ http_sfv/util_binary.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from enum import IntEnum
 from typing import Tuple, Union
 
 from .decimal import FRAC_DIGITS, PRECISION
@@ -27,25 +28,71 @@ HEADER_MASK = 0b00001111
 
 
 def parse_binary(data: bytes) -> Tuple[int, StructuredFieldValue]:
-    try:
-        return _top_level_parsers[data[0] >> HEADER_BITS](data)  # type: ignore
-    except KeyError:
-        return parse_item(data)
+    stype = data[0] >> HEADER_BITS
+    if stype == StructuredType.DICTIONARY:
+        return parse_dictionary(data)
+    if stype == StructuredType.LIST:
+        return parse_list(data)
+    return parse_item(data)
 
 
 def ser_binary(structure: StructuredFieldValue) -> bytearray:
-    structure_type = structure.__class__
-    try:
-        return _top_level_serialisers[structure_type][1](structure)  # type: ignore
-    except KeyError:
-        return _serialisers[structure_type][1](structure)  # type: ignore
+    if isinstance(structure, Dictionary):
+        return ser_dictionary(structure)
+    if isinstance(structure, List):
+        return ser_list(structure)
+    return ser_item(structure)
+
+
+def parse_list(data: bytes) -> Tuple[int, List]:
+    """
+    Payload: Integer l, l items or inner lists following
+    """
+    output = List()
+    bytes_consumed, member_count = _decode_integer(HEADER_BITS, data)
+    for i in range(member_count):
+        offset, member = parse_item_or_inner_list(data[bytes_consumed:])
+        bytes_consumed += offset
+        output.append(member)
+    return bytes_consumed, output
+
+
+def ser_list(value: List) -> bytearray:
+    data = bytearray(b"")
+    return add_type(data, StructuredType.LIST)
+
+
+def parse_dictionary(data: bytes) -> Tuple[int, Dictionary]:
+    """
+    Payload: Integer num, num x (Integer keyLen, structure) pairs
+    """
+    output = Dictionary()
+    bytes_consumed, member_count = _decode_integer(HEADER_BITS, data)
+    for i in range(member_count):
+        offset, keyLength = _decode_integer(0, data[bytes_consumed:])
+        bytes_consumed += offset
+        key_end = bytes_consumed + offset
+        name = data[bytes_consumed:key_end].decode("ascii")
+        bytes_consumed = key_end
+        offset, value = parse_item_or_inner_list(data[bytes_consumed:])
+        bytes_consumed += offset
+        output[name] = value
+        # FIXME: Parameters
+    return bytes_consumed, output
+
+
+def ser_dictionary(value: Dictionary) -> bytearray:
+    data = _encode_integer(HEADER_BITS, len(value))
+    for member in value:
+        pass # TODO
+    return add_type(data, StructuredType.DICTIONARY)
 
 
 def parse_item_or_inner_list(data: bytes) -> Tuple[int, Union[InnerList, Item]]:
-    try:
-        return _item_or_inner_list_parsers[data[0] >> HEADER_BITS](data)  # type: ignore
-    except KeyError:
-        raise
+    stype = data[0] >> HEADER_BITS
+    if stype == StructuredType.INNER_LIST:
+        return parse_inner_list(data)
+    return parse_bare_item(data)
 
 
 def ser_item_or_inner_list(structure: Union[InnerList, Item]) -> bytearray:
@@ -53,10 +100,20 @@ def ser_item_or_inner_list(structure: Union[InnerList, Item]) -> bytearray:
 
 
 def parse_bare_item(data: bytes) -> Tuple[int, BareItemType]:
-    try:
-        return _bare_item_parsers[data[0] >> HEADER_BITS](data)  # type: ignore
-    except KeyError:
-        raise
+    stype = data[0] >> HEADER_BITS
+    if stype == StructuredType.INTEGER:
+        return parse_integer(data)
+    if stype == StructuredType.DECIMAL:
+        return parse_decimal(data)
+    if stype == StructuredType.BOOLEAN:
+        return parse_boolean(data)
+    if stype == StructuredType.BYTESEQ:
+        return parse_byteseq(data)
+    if stype == StructuredType.STRING:
+        return parse_string(data)
+    if stype == StructuredType.TOKEN:
+        return parse_token(data)
+    raise ValueError
 
 
 def ser_bare_item(item: BareItemType) -> bytearray:
@@ -72,42 +129,56 @@ def parse_item(data: bytes) -> Tuple[int, Item]:
 
 
 def ser_item(item: Item) -> bytearray:
-    structure_type = item.__class__
-    return _serialisers[structure_type][1](item)  # type: ignore
+    if isinstance(item, InnerList):
+        return ser_inner_list(item.value)
+    if isinstance(item, Token):
+        return ser_token(item)
+    if isinstance(item.value, int):
+        return ser_integer(item.value)
+    if isinstance(item.value, Decimal):
+        return ser_decimal(item.value)
+    if isinstance(item.value, bool):
+        return ser_boolean(item.value)
+    if isinstance(item.value, bytes):
+        return ser_byteseq(item.value)
+    if isinstance(item.value, str):
+        return ser_string(item.value)
+    raise ValueError
 
 
 def parse_integer(data: bytes) -> Tuple[int, int]:
     """
     Payload: Integer i
     """
+    ## TODO: sign
     return _decode_integer(HEADER_BITS, data)
 
 
 def ser_integer(value: int) -> bytearray:
-    data = _encode_integer(HEADER_BITS + 1, value)
-    data = add_type(data, INTEGER)
     ## TODO: add sign
-    return data
+    data = _encode_integer(HEADER_BITS + 1, value)
+    return add_type(data, StructuredType.INTEGER)
 
 
 def parse_decimal(data: bytes) -> Tuple[int, Decimal]:
     """
     Payload: Integer i, Integer f - indicating the integer and fractional components in use
     """
+    ## TODO: sign
     bytes_consumed, integer_component = _decode_integer(HEADER_BITS, data)
     offset, fractional_component = _decode_integer(0, data[bytes_consumed:])
     return bytes_consumed + offset, Decimal()  # FIXME
 
 
 def ser_decimal(value: Decimal) -> bytearray:
+    ## TODO: sign
     input_decimal = round(value, FRAC_DIGITS)
     abs_decimal = value.copy_abs()
-    integer_component_s = str(int(abs_decimal))
-    fractional_component = abs_decimal.quantize(PRECISION).normalize() % 1
+    integer_component = int(abs_decimal)
+    fractional_component = int(str(abs_decimal.quantize(PRECISION).normalize() % 1)[2:])
     data = _encode_integer(integer_component, HEADER_BITS)
-    data = add_type(data, DECIMAL)
     data += _encode_integer(fractional_component, 0)
-    return data
+    return add_type(data, StructuredType.DECIMAL)
 
 
 def parse_boolean(data: bytes) -> Tuple[int, bool]:
@@ -119,10 +190,10 @@ def parse_boolean(data: bytes) -> Tuple[int, bool]:
 
 def ser_boolean(value: bool) -> bytearray:
     if value:
-        data = 0b00010000
+        data = bytearray(b'\x10')
     else:
-        data = 0b00000000
-    return add_type(data, BOOLEAN)
+        data = bytearray(b'\x00')
+    return add_type(data, StructuredType.BOOLEAN)
 
 
 def parse_byteseq(data: bytes) -> Tuple[int, bytes]:
@@ -135,7 +206,9 @@ def parse_byteseq(data: bytes) -> Tuple[int, bytes]:
 
 
 def ser_byteseq(value: bytes) -> bytearray:
-    pass
+    data = _encode_integer(HEADER_BITS, len(value))
+    data += value
+    return add_type(data, StructuredType.BYTESEQ)
 
 
 def parse_string(data: bytes) -> Tuple[int, str]:
@@ -148,7 +221,9 @@ def parse_string(data: bytes) -> Tuple[int, str]:
 
 
 def ser_string(value: str) -> bytearray:
-    pass
+    data = _encode_integer(HEADER_BITS, len(value))
+    data += value.encode("ascii")
+    return add_type(data, StructuredType.STRING)
 
 
 def parse_token(data: bytes) -> Tuple[int, Token]:
@@ -161,47 +236,9 @@ def parse_token(data: bytes) -> Tuple[int, Token]:
 
 
 def ser_token(value: Token) -> bytearray:
-    pass
-
-
-def parse_list(data: bytes) -> Tuple[int, List]:
-    """
-    Payload: Integer l, l items or inner lists following
-    """
-    output = List()
-    bytes_consumed, members = _decode_integer(HEADER_BITS, data)
-    for i in range(members):
-        offset, member = parse_item_or_inner_list(data[bytes_consumed:])
-        bytes_consumed += offset
-        output.append(member)
-    return bytes_consumed, output
-
-
-def ser_list(value: List) -> bytearray:
-    pass
-
-
-def parse_dictionary(data: bytes) -> Tuple[int, Dictionary]:
-    """
-    Payload: Integer num, num x (Integer keyLen, structure) pairs
-    """
-    output = Dictionary()
-    bytes_consumed, members = _decode_integer(HEADER_BITS, data)
-    for i in range(members):
-        offset, keyLength = _decode_integer(0, data[bytes_consumed:])
-        bytes_consumed += offset
-        key_end = bytes_consumed + offset
-        name = data[bytes_consumed:key_end].decode("ascii")
-        bytes_consumed = key_end
-        offset, value = parse_item_or_inner_list(data[bytes_consumed:])
-        bytes_consumed += offset
-        output[name] = value
-    # FIXME: Parameters
-    return bytes_consumed, output
-
-
-def ser_dictionary(value: Dictionary) -> bytearray:
-    pass
+    data = _encode_integer(HEADER_BITS, len(value))
+    data += value.encode("ascii")
+    return add_type(data, StructuredType.TOKEN)
 
 
 def parse_inner_list(data: bytes) -> Tuple[int, InnerList]:
@@ -244,54 +281,21 @@ def ser_parameters(value: Parameters) -> bytearray:
     pass
 
 
-def add_type(data: bytearray, sf_type: int) -> bytearray:
+def add_type(data: bytearray, sf_type: StructuredType) -> bytearray:
     data[0] = (sf_type << HEADER_BITS) | data[0]
     return data
 
 
-DICTIONARY = 1
-LIST = 2
-INNER_LIST = 3
-INTEGER = 4
-DECIMAL = 5
-BOOLEAN = 6
-BYTESEQ = 7
-STRING = 8
-TOKEN = 9
-
-
-_top_level_parsers = {DICTIONARY: parse_dictionary, LIST: parse_list}
-_item_or_inner_list_parsers = {
-    INNER_LIST: parse_inner_list,
-    INTEGER: parse_integer,
-    DECIMAL: parse_decimal,
-    BOOLEAN: parse_boolean,
-    BYTESEQ: parse_byteseq,
-    STRING: parse_string,
-    TOKEN: parse_token,
-}
-_bare_item_parsers = {
-    INTEGER: parse_integer,
-    DECIMAL: parse_decimal,
-    BOOLEAN: parse_boolean,
-    BYTESEQ: parse_byteseq,
-    STRING: parse_string,
-    TOKEN: parse_token,
-}
-
-_top_level_serialisers = {
-    Dictionary: (DICTIONARY, ser_dictionary),
-    List: (LIST, ser_list)
-}
-_serialisers = {
-    InnerList: (INNER_LIST, ser_inner_list),
-    int: (INTEGER, ser_integer),
-    Decimal: (DECIMAL, ser_decimal),
-    bool: (BOOLEAN, ser_boolean),
-    bytes: (BYTESEQ, ser_byteseq),
-    str: (STRING, ser_string),
-    Token: (TOKEN, ser_token),
-}
+class StructuredType(IntEnum):
+    DICTIONARY = 1
+    LIST = 2
+    INNER_LIST = 3
+    INTEGER = 4
+    DECIMAL = 5
+    BOOLEAN = 6
+    BYTESEQ = 7
+    STRING = 8
+    TOKEN = 9
 
 
 #### From hyper hpack
@@ -299,7 +303,7 @@ _serialisers = {
 # Precompute 2^i for 1-8 for use in prefix calcs.
 # Zero index is not used but there to save a subtraction
 # as prefix numbers are not zero indexed.
-_PREFIX_BIT_MAX_NUMBERS = [(2 ** i) - 1 for i in range(9)]
+_PREFIX_BIT_MAX_NUMBERS = [(2**i) - 1 for i in range(9)]
 
 
 def _decode_integer(prefix_bits: int, data: bytes) -> Tuple[int, int]:
