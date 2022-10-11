bd3e5009bc08842ed655226c0d83f8fd092068ac
Mark Nottingham <mnot@mnot.net>
2022-10-11 20:26:21 +1100

    alias properly

--- http_sfv/types.py
+++ http_sfv/types.py
@@ -1,21 +1,22 @@
 from collections import UserString
 from decimal import Decimal
 from typing import Union, Dict, List, Tuple
+from typing_extensions import TypeAlias
 
 
 class Token(UserString):
     def __init__(self, seq: str):
         self.data = seq
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Token("{self.data}")'
 
 
-BareItemType = Union[int, float, str, bool, Decimal, bytes, Token]
-ParamsType = Dict[str, BareItemType]
-ItemType = Tuple[BareItemType, ParamsType]
-InnerListType = Tuple[List[ItemType], ParamsType]
-ItemOrInnerListType = Union[ItemType, InnerListType]
-ListType = List[Union[ItemType, InnerListType]]
-DictionaryType = Dict[str, Union[ItemType, InnerListType]]
-StructuredType = Union[ItemType, ListType, DictionaryType]
+BareItemType: TypeAlias = Union[int, float, str, bool, Decimal, bytes, Token]
+ParamsType: TypeAlias = Dict[str, BareItemType]
+ItemType: TypeAlias = Union[BareItemType, Tuple[BareItemType, ParamsType]]
+InnerListType: TypeAlias = Union[List[ItemType], Tuple[List[ItemType], ParamsType]]
+ItemOrInnerListType: TypeAlias = Union[ItemType, InnerListType]
+ListType: TypeAlias = List[Union[ItemType, InnerListType]]
+DictionaryType: TypeAlias = Dict[str, Union[ItemType, InnerListType]]
+StructuredType: TypeAlias = Union[ItemType, ListType, DictionaryType]
